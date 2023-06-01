e5aa1676cc286b99b1faeb2b92bfffc3d7bf8ce5
ilyafaybisovich <ilya.faybisovich@gmail.com>
2023-06-01 15:14:14 +0200

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  Navbar,
+  Feed,
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => {
-  return <div>Appy</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id" element={<VideoDetail />}></Route>
+          <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+          <Route path="/search/:searchTerm " element={<SearchFeed />}></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
